b903641b57c1e458e96eb61ba6f9ffb6e2b68c65
TMFrancis <BostonTomF@gmail.com>
2022-04-04 11:59:18 -0400

    working version that writes plotting dataframe without sum of all matrices at bottom (which is the next task)

--- Xie_Matrix_last_update_4_2.R
+++ Xie_Matrix_last_update_4_2.R
@@ -11,15 +11,15 @@ input_df = read.csv("data/example_data.csv", header = FALSE, stringsAsFactors = 
 # ----------------------------------------------------------
 
 # info about input_df
-# x_categories <- input_df[3:nrow(input_df),'V2']     # number of rows to be looped
-categories <- input_df[2,3:ncol(input_df)]        # categories
+categories <- input_df[2,3:ncol(input_df)]        # categories to be analyzed
 
 # create dataframe without text
+# expect to remove lines 16-20 when analyzing multiple matrices
 data_df <- input_df[3:nrow(input_df),3:ncol(input_df)]
 v_nc <- c(1:ncol(data_df))
 data_df[,v_nc] <- apply(data_df[ ,v_nc],2,function(x) as.numeric(as.character(x)))
 
-# create dataframe to be plotted
+# create empty dataframe to be used by ggplot 
 basic_df <- data.frame(Time1 = c(''),          # interval start
                        Time2 = c(''),         # interval end
                        persistence = c(''),   # diagonal value (was diagonal_label)
@@ -28,74 +28,49 @@ basic_df <- data.frame(Time1 = c(''),          # interval start
                        label = c(''))         # interval label
 matrix_df <- basic_df[-c(1),]                  # remove first blank line
 
-# populate dataframe to be plotted
-for (i in 1:length(input_df[,2])) {
-  if (is.element(input_df[i,2], c("")) & !is.element(input_df[i,3], categories)) { #row has Y date
-    time_2 <- input_df[i,3]
-    time_1 <- input_df[(i+2),1]
-    time_gap <- as.numeric(time_2) - as.numeric(time_1)
+# populate dataframe to be plotted - matrix_df
+r_count <- 0                                                                       # set counter
+for (i in 1:length(input_df[,2])) {                                                # loop thru each row of input_df
+  if (is.element(input_df[i,2], c("")) & !is.element(input_df[i,3], categories)) { # row has Y date
+    time_2 <- input_df[i,3]                                                        # Y date
+    time_1 <- input_df[(i+2),1]                                                    # X date
+    time_gap <- as.numeric(time_2) - as.numeric(time_1)                            # time interval
     next
   }
-  if (is.element(input_df[i,2], c("")) & is.element(input_df[i,3], categories)) {  #row has Y categories
-    next
+  if (is.element(input_df[i,2], c("")) & is.element(input_df[i,3], categories)) {  # row has Y categories
+    temp_df <- input_df[(i+1):(i+length(categories)), c(3:(length(categories)+2))] # temporary df for current matrix
+    next                                                                           # skip to next row
   }
   # rows with data
-  for (y in 1:length(categories)) {
+  for (y in 1:length(categories)) {                                                # loop through categories
+    if (y == 1) {r_count <- r_count + 1}                                           # increment counter
     # populate category names to var1 & var2 columns
-    matrix_df[nrow(matrix_df) + 1,'Time1'] <- input_df[i,2]
-    matrix_df[nrow(matrix_df),'Time2'] <- categories[y]
+    matrix_df[nrow(matrix_df) + 1,'Time1'] <- input_df[i,2]                        # write Time1 column
+    matrix_df[nrow(matrix_df),'Time2'] <- categories[y]                            # write Time2 column
     # populate interval label to time column
-    matrix_df[nrow(matrix_df),'label'] <- paste(time_1,time_2, sep="-")
+    matrix_df[nrow(matrix_df),'label'] <- paste(time_1,time_2, sep="-")            # write matrix label
     # calculate & populate percentage column
-    matrix_df[nrow(matrix_df),'percent'] <- 
-      (input_df[i,y+3]/(time_gap*sum(as.numeric(input_df[i,3:(2+length(categories))]))))*100
-    
-    
-  }    # end of for y loop
-}      # end of for i loop
-
-# rest of code through QA is for reference    
-    for (y in 1:length(categories)) {
-      # populate category names to var1 & var2 columns
-      matrix_df[nrow(matrix_df) + 1,'Time1'] <- data_df[i,2]
-      matrix_df[nrow(matrix_df),'Time2'] <- categories[y]
-        matrix_df[nrow(matrix_df),'percent'] <- 
-        (input_df[i,y+1]/(time_gap*sum(input_df[1:length(x_categories),1:length(y_categories)+1])))*100
-    }
-
-  
-  for (y in 1:length(categories)) {
-    # populate category names to var1 & var2 columns
-    matrix_df[nrow(matrix_df) + 1,'Time1'] <- x_categories[i]
-    matrix_df[nrow(matrix_df),'Time2'] <- y_categories[y]
-    # populate interval label to time column
-    matrix_df[nrow(matrix_df),'label'] <- paste(time_1,time_2, sep="-")
-    # calculate & populate percentage column
-    matrix_df[nrow(matrix_df),'percent'] <- 
-      (input_df[i,y+1]/(time_gap*sum(input_df[1:length(x_categories),1:length(y_categories)+1])))*100
-    # calculate & populate intensity column
-    matrix_df[nrow(matrix_df),'intensity'] <- 
-      (input_df[i,y+1]/(time_gap*sum(input_df[i,1:length(y_categories)+1]))) - 
-      (sum(input_df[y+1]) - input_df[y,y+1])/ (time_gap*(sum(input_df[1:length(x_categories),1:length(y_categories)+1])- sum(input_df[y+1])))
-    # re-populate diagonals = 0
-    if (matrix_df[nrow(matrix_df),'Time1'] == matrix_df[nrow(matrix_df),'Time2']) {
+    matrix_df[nrow(matrix_df),'percent'] <-                                        # write percentage
+      (as.numeric(temp_df[r_count, y])) /                                          # cell
+      (time_gap*sum(as.numeric(temp_df[r_count,]))) * 100                          # time interval * sum of row
+    matrix_df[nrow(matrix_df),'intensity'] <-                                      # write intensity
+      (as.numeric(temp_df[r_count, y])) /                                          # cell
+      ((time_gap*sum(as.numeric(temp_df[r_count,]))) * 100) -                      # time interval * sum of row
+      (sum(as.numeric(temp_df[y,])) -                                              # sum of column
+      as.numeric(temp_df[y,y])) /                                                  # diagonal (agreement)
+      (time_gap *                                                                  # time interval
+      (sum(as.numeric(temp_df[y,])) -                                              # sum of column
+       sum(as.numeric(unlist(temp_df)))))                                          # sum of dataframe (extent)
+    if (matrix_df[nrow(matrix_df),'Time1'] == matrix_df[nrow(matrix_df),'Time2']) {# write diagonal labels
       matrix_df[nrow(matrix_df),'persistence'] <- 1
       matrix_df[nrow(matrix_df),'percent'] <- 0
       matrix_df[nrow(matrix_df),'intensity'] <- 0
-    }
-    # calculate intensity
-    #v_int <- as.numeric(input_df[1,3]) - as.numeric(input_df[3,1])                                  # number of periods in interval
-    # intensity deviation
-    
-    # QA
-    cat('Cell value for row', i, 'and column', y, ': ', input_df[i+2,y+2], '\n')
-    cat('Sum of row:', sum(as.numeric(input_df[(i+2),3:nrow(input_df)])),'\n')
-    cat('Sum of column:', sum(as.numeric(input_df[3:ncol(input_df),(y+2)])),'\n')
-    cat('Persistence:', as.numeric(input_df[(y+2),(y+2)]), '\n')
-    #print(v_w1)
-    #print(v_w2)
-  }
-}
+    } # end write diagonal labels 
+  }   # end loop y
+  if (r_count == length(categories)) {r_count <- 0}                                # reset counter
+}     # end loop i 
+
+#-------------must add sum at bottom of matrix_df-------------------------------
 
 # organize data frame formatting
 final_df <- matrix_df[1:16,]
